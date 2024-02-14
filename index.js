cddf1667c9abe94d8576d2fb6b5f68f16f4f65d4
three <45986157+ewlondon@users.noreply.github.com>
2024-02-13 21:11:54 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-const buttons = document.querySelectorAll("button");
 const closeBtns = document.querySelectorAll(".close-btn");
 const openBtns = document.querySelectorAll(".open-btn");
-
+const h2Els = document.querySelectorAll("h2");
 const details = document.querySelectorAll("p");
 
-buttons.forEach((b, index) => {
-  b.addEventListener("click", (e) => {
+h2Els.forEach((h, index) => {
+  h.addEventListener("click", (e) => {
     details[index].classList.toggle("hidden");
     closeBtns[index].classList.toggle("hidden");
     openBtns[index].classList.toggle("hidden");
